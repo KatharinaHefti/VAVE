a7cd5db411bee6af57cb73fab60ba55282e6ec56
KHefti <k_hefti@icloud.com>
2020-06-18 12:48:50 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VAVE sports and camps</title>
+    <!-- style -->
+    <link rel="stylesheet" href="style.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- google font PT Sans -->
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Caption:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+  <?php include 'includes/nav.php';?>
+        
+    <!-- wave1 for background navigation -->
+    <img id="wave1" src="img/wave1.svg" alt="wave"> 
+
+    <!-- main grid container -->
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-1">
+          </div>
+          <div class="col-lg-6">
+            <main>
+                <h4>Sports and camps</h4>
+                <h1>This is an inspirational sporty intro to your page lorem.</h1>
+                <p>Try to finde one sentence or two to describe the concept of your page. Sports and camps describe a mood to make people feel whatever bla bla.</p>
+                <button>join a training</button>
+            </main>
+          </div>
+          <div class="col-lg-4">
+            <img class="shell" src="img/shell.svg" alt="shell">
+          </div>
+          <div class="col-lg-1">
+          </div>
+
+        </div>
+      </div>
+
+    <!-- section grid container -->
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-1"></div>
+          <div class="col-lg-10">
+            <section>
+                <h4>Gallery</h4>
+                <h2>Something about your Gallery Loerum ipsum dolor sit.</h2>
+                <p>Lead text for your gallery. Describe the gallerys Whats. Lead text for your gallery. Describe the gallerys WhatsLead text for your gallery. Describe the gallerys Whats </p>
+
+                <div class="flexer">
+                    <a class="circle" href="gallery-training.html"><img src="img/icon/icon_sport.svg" alt="sport icon"></a>
+                    <a class="circle" href="gallery-camp.html"><img src="img/icon/icon_camp.svg" alt="camp icon"></a>
+                    <a class="circle" href="gallery-event.html"><img src="img/icon/icon_event.svg" alt="event icon"></a>
+                </div>
+            </section>
+          </div>
+          <div class="col-lg-1"></div>
+        </div>
+      </div>
+
+      <img id="wave2" src="img/wave2.svg" alt="wave">
+
+
+    <!-- Events -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-4">
+            <img class="flyer" src="img/VAVE-Flyer.png" alt="Flyer">
+          </div>
+          <div class="col-lg-6">
+            <section>
+                <h4>Events</h4>
+                    <h2>Next Event Promo headline loerum ipsum donor</h2>
+                    <p>Lead text for your event. Loerum Epsom dollar sit amen. Lead text for your event.  Loerum Epsom dollar sit amen. Lead text for your event. Loerum Epsom dollar sit amen.</p>
+                    <div class="container-left">
+                        <img class="icon-s" src="img/icon/icon_event.svg" alt="event icon">
+                        <h5>Thursday, 23.06.2020</h5>
+                    </div>
+                    <div class="container-left">
+                        <img class="icon-s" src="img/icon/icon_time.svg" alt="time icon">
+                        <h5>12:00 until 14:00 </h5>
+                    </div>
+                    <div class="container-left">
+                        <img class="icon-s" src="img/icon/icon_location.svg" alt="location icon">
+                        <h5>8003 Zürich, Street 33</h5>
+                    </div>
+                    <p>Get your tickets before it’s too late. Loerum ipsum dolor sit amen.  Loerum ipsum dolor sit ame ipsum dolor sit amen.</p>
+                    <div class="flexer">
+                        <button><a href=""></a>Get your ticket</button>
+                        <p>just 3 tickets left</p>
+                    </div> 
+            </section>
+          </div>
+          <div class="col-lg-1"></div>
+        </div>
+      </div>
+
+       <!-- Location -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-4">
+                <h4>Location</h4>
+                <h2>Where you find me</h2>
+                <p>Lead text for your location. Describe the room you are in. Whats specials there and what is unique you can offer loerum ipsum dolor sit amen. </p> 
+          </div>
+          <div class="col-lg-6">
+            <img class="icon-l" src="img/icon/icon_location.svg" alt="time icon">
+
+                <img class="roundPic" src="img/pic/studio.png" alt="Valeria">
+                
+
+          </div>
+          <div class="col-lg-1"></div>
+        </div>
+      </div>
+
+
+
+    <!-- Footer -->
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-1"></div>
+          <div class="col-lg-4">
+        
+                <img class="logo-sm" src="img/VAVE-logo.svg" alt="VAVE logo">
+                <p>Valeria Verzar</p> 
+                <p>Badenerstr. 68</p> 
+                <p>CH- 8004 Zürich</p> 
+                <p>Email: v.verzar@yahoo.com</p> 
+                <p>Tel: 000 000 00 00</p> 
+
+            </div>
+            <div class="col-lg-2">
+                <p><a href="">About</a></p>
+                <p><a href="">Training</a></p>
+                <p><a href="">Impressum</a></p>
+                <p><a href="">Terms & Condition</a></p>
+            </div>
+            <div class="col-lg-4">
+                <p>Social media</p>
+                <div class="flexer">
+                    
+                    <img class="icon-s" src="img/icon/icon-social_youtube.svg" alt="youtube icon">
+                    <a href="https://www.facebook.com/bjjsurfcamp18"><img class="icon-s" src="img/icon/icon-social_facebook.svg" alt="facebook icon"></a>
+                    <a href="https://www.instagram.com/surfbjjcamps/?hl=de"><img class="icon-s" src="img/icon/icon-social_insta.svg" alt="instagram icon"></a>
+                </div>
+            </div>
+            
+          <div class="col-lg-1"></div>
+        </div>
+      </div>
+
+    <img id="wave3" src="img/wave3.svg" alt="">  
+    
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+
+</body>
+</html>
