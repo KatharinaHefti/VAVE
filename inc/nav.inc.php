1cac03e07539d8e46278cb95dc388fae220bf821
KHefti <k_hefti@icloud.com>
2020-08-13 19:57:54 +0200

    add background wave to navigation

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,18 +1,26 @@
+<style>
+  nav{
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .brand{
+    height: 30px;
+    display: flex;
+  }
+
+  .bg{
+    position: absolute;
+    top: 0;
+    z-index: -1;
+  } 
+</style>
+
 <nav>
   <!-- brand -->
   <img class="brand" src="./img/brand.svg" alt="VAVE logo">
-  
-  <!-- dropdown -->
-  <div class="dropdown">
-    <!-- icon menu -->
-    <svg onclick="myFunction()" class="dropbtn"><use xlink:href="#icon_menu"></use></svg>  
-    <div id="myDropdown" class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 1</a>
-      <a href="#">Link 1</a>
-    </div>
-  </div>
+  <svg class="small"><use xlink:href="#icon_menu"></use></svg>  
 </nav>
-
-<!-- js for navigation dropdown -->
-<script  src="./js/nav.dropdown.js" ></script>
+<img class="bg" src="./img/wave/wave1.svg" alt="wave">
--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php 
 include ("./inc/header.inc.php"); 
 include ("./inc/nav.inc.php"); 
+?>
 
+  <main>
+    <h4>Sports and camps</h4>
+    <h1>This is an inspirational sporty intro to your page lorem.</h1>
+    <p>Try to finde one sentence or two to describe the concept of your page. Sports and camps describe a mood to make people feel whatever bla bla.</p>
+    <button>join a training</button>
+  </main>
+
+<?php 
 include ("./inc/footer.inc.php"); 
 ?>
 
