b72483417bd9f124348652f595e1b5003b975ea2
KHefti <k_hefti@icloud.com>
2020-08-13 18:43:09 +0200

    add footer basic

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,8 +1,47 @@
-    
-    <footer>
-        <ul>
-            <li><a href="">link</a></li>
-        </ul>
-    </footer>
+  <footer>
+
+    <!-- Contact -->
+    <div>
+    <h4>contact</h4>
+    <img class="small" src="./img/brand.svg" alt="">
+    <p class="paint-white">Valeria Verzar <br>
+          Badenerstr. 68<br>
+          CH-8004 Zürich<br>
+          Email: v.verzar@yahoo.com<br>
+          Tel: 000 000 00 00</p> 
+    </div>
+
+    <!-- Links -->
+    <div>
+      <p>
+        <a href="me.php">About</a></br>
+        <a href="training.php">Training</a></br>
+        <a href="impressum.php">Impressum</a></br>
+        <a href="terms.php">Terms & Condition</a>
+      </p>
+    </div>
+
+    <!-- Social Media -->
+    <div class="social">
+      <!-- youtube -->
+      <a href="https://www.youtube.com/channel/UC5MMAPA-e-DRb7n2s_dVyng">
+            <svg class="small">
+              <use xlink:href="#icon-social_youtube"></use>
+            </svg> 
+          </a>   
+          <!-- facebook -->
+          <a href="https://www.facebook.com/bjjsurfcamp18">
+            <svg class="small">
+              <use xlink:href="#icon-social_facebook"></use>
+            </svg> 
+          </a>
+          <!-- instagram -->
+          <a href="https://www.instagram.com/surfbjjcamps/?hl=de">
+            <svg class="small">
+              <use xlink:href="#icon-social_insta"></use>
+            </svg> 
+          </a>
+    </div>
+  </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php 
 include ("./inc/header.inc.php"); 
 include ("./inc/nav.inc.php"); 
-?>
 
-<p>hallo world</p>
-<?php
 include ("./inc/footer.inc.php"); 
 ?>
 
