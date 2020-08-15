3063533d92426c78a04b1f54f747e7a2ab42cfa8
KHefti <k_hefti@icloud.com>
2020-08-15 13:51:13 +0200

    add after shock

---
+++ inc/main.inc.php
@@ -0,0 +1,10 @@
+<main>
+    <h5>Sports and camps</h5>
+    <br>
+    <h1>This is an inspirational sporty intro to your page lorem.</h1>
+    <br>
+    <p>Try to finde one sentence or two to describe the concept of your page. Sports and camps describe a mood to make people feel whatever bla bla.</p>
+    <button><a href="">join a training</a></button>
+</main>
+
+<link rel="stylesheet" href="style/style.css">
--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <?php 
 include ("./inc/header.inc.php"); 
-include ("./inc/nav.inc.php"); 
-?>
+include ("./inc/nav.inc.php"); ?>
+
+<html>
+<link rel="stylesheet" href="./style/style.css">
+
+</html>
+
+<?php
+/* * * * * * * * * * * * * * * * * * * * main * * * * * * * * * * * * * * * * * * * */
+
+// Main Text Variables
+$MainChapterTitle = 'Sports and camps';
+$MainHeadline = 'This is an inspirational sporty intro to your page lorem.';
+$MainPragraph = 'Try to finde one sentence or two to describe the concept of your page. Sports and camps describe a mood to make people feel whatever bla bla.';
+$MainLink = '#';
+$MainButton = 'join a training';
+
+include ("./inc/main.inc.php"); 
+
+/* * * * * * * * * * * * * * * * * * * * gallery section * * * * * * * * * * * * * * * * * * * */
+
+$GallerySectionChapterTitle = 'Gallery';
+$GallerySectionHeadline = 'Something about your GalleryLoerum ipsum dolor sit.';
+include ("./inc/gallerySection.inc.php"); 
+
+/* * * * * * * * * * * * * * * * * * * * xxxx * * * * * * * * * * * * * * * * * * * */
+
+include ("./inc/main.inc.php"); 
+include ("./inc/main.inc.php"); 
+
+/* * * * * * * * * * * * * * * * * * * * footer * * * * * * * * * * * * * * * * * * * */
 
 
-  <main>
-    <h4>Sports and camps</h4>
-    <br>
-    <h1>This is an inspirational sporty intro to your page lorem.</h1>
-    <br>
-    <p>Try to finde one sentence or two to describe the concept of your page. Sports and camps describe a mood to make people feel whatever bla bla.</p>
-    <button>join a training</button>
-  </main>
-
-<?php 
 include ("./inc/footer.inc.php"); 
 ?>
-
