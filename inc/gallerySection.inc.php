52feff1aba8bba2dc4f48cd8a12fd9169e6dbc97
KHefti <k_hefti@icloud.com>
2020-08-15 13:39:28 +0200

    add gallery section template

---
+++ inc/gallerySection.inc.php
@@ -0,0 +1,33 @@
+
+
+<section class="gallery">
+  <h5 class="paint-white"><?php echo $GallerySectionChapterTitle?></h5>
+  <br>
+  <h1 class="paint-white"><?php echo $GallerySectionHeadline?></h1>
+  <br>
+
+  <!-- gallery icons -->
+  <div class="flexer">
+    <a class="circle" href="gallery-training.html">
+      <svg class="iconBig">
+        <use xlink:href="#icon_sport"></use>
+      </svg>
+    </a>
+
+    <a class="circle" href="gallery-camp.html">
+      <svg class="iconBig">
+        <use xlink:href="#icon_camp"></use>
+      </svg>
+    </a>
+
+    <a class="circle" href="gallery-event.html">
+      <svg class="iconBig">
+        <use xlink:href="#icon_event"></use>
+      </svg>
+    </a>
+  </div>
+  <!-- menu wave background picture 
+  <img class="waveBg2" src="./img/wave/wave2.svg" alt="wave">-->
+
+</section>
+
