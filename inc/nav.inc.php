6b99ae42522e2b543925822cfcbcfd1649095193
KHefti <k_hefti@icloud.com>
2020-08-16 19:36:06 +0200

    fix nav brand link

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,6 +1,6 @@
 <nav>
   <!-- brand -->
-  <img class="brand" src="./img/brand.svg" alt="VAVE logo">
+  <a href="./index.php"><img class="brand" src="./img/brand.svg" alt="VAVE logo"></a>
 
   <!-- dropdown menu -->
   <svg id="menuIcon" class="small"><use xlink:href="#icon_menu"></use></svg>
