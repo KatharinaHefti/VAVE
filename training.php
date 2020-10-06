fb7abc6518c2b5df19e8ec6875caab0d7c215a38
KHefti <k_hefti@icloud.com>
2020-10-06 22:22:00 +0200

    fix header picture

--- training.php
+++ training.php
@@ -3,9 +3,8 @@ include ("./inc/header.inc.php");
 include ("./inc/nav.inc.php"); 
 /* * * * * * * * * * * * * * * * * * * * main * * * * * * * * * * * * * * * * * * * */
 ?>
-
+<style><?php include "style/elements/picture.style.css" ?></style>
 <img class="headerPic" src="img/training/training_header.jpg" alt="valeria verzar">
-
 <section class="gallery">
   <h4 class="paint-white">Training</h4>
   <br>
